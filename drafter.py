97ae93e74d9d8228eb916a414f97560160e073d8
MeKiGiWi <danyagenij@gmail.com>
2024-11-18 20:05:17 +0300

    some math magic

--- drafter.py
+++ drafter.py
@@ -10,5 +10,12 @@ with open('data.json', 'r') as f:
     plt.plot([x[0] for x in database], [x[1] for x in database])
 with open('our_data.json', 'r') as f:
     database = load(f)
-    plt.plot([x[0] for x in database], [x[1] for x in database], color='r')
+    plt.plot([x[0] for x in database], [x[3] for x in database], color='r')
+#     fig, ax = plt.subplots(4)
+#     def mat(x):
+#         return 0 <= x <= 500
+#     ax[0].plot([x[0] for x in database if mat(x[0])], [x[1] for x in database if mat(x[0])])
+#     ax[1].plot([x[0] for x in database if mat(x[0])], [x[2] for x in database if mat(x[0])], color='r')
+#     ax[2].plot([x[0] for x in database if mat(x[0])], [x[3] for x in database if mat(x[0])], color='y')
+#     ax[3].plot([x[0] for x in database if mat(x[0])], [x[4] for x in database if mat(x[0])], color='g')
 plt.show()
--- math_part.py
+++ math_part.py
@@ -4,25 +4,52 @@ import math
 from time import sleep
 
 
+def temp_by_elev(height):
+    # temperature depending by elevating
+    if (0 <= height <= 3000):
+        temperature = 288.2 - 3.2 * height / 1000
+    elif (3000 <= height <= 11000):
+        temperature = 268.7 - 6.5 * (height / 1000 - 3)
+    elif (11000 <= height <= 20000):
+        temperature = 216.7
+    elif (20000 <= height <= 32000):
+        temperature = 216.7 + (height / 1000 - 20)
+    elif (32000 <= height <= 40000):
+        temperature = 228.5 + 2.75 * (height / 1000 - 32)
+    elif (40000 <= height <= 50000):
+        temperature = 250.4 + 2 * (height / 1000 - 40)
+    elif (50000 <= height <= 60000):
+        temperature = 270.7 - 2.3 * (height / 1000 - 50)
+    elif (60000 <= height <= 80000):
+        temperature = 247 - 2.45 * (height / 1000 - 60)
+    elif (80000 <= height <= 100_000):
+        temperature = 198.6 - 0.1 * (height / 1000 - 80)
+    elif (100_000 <= height <= 150_000):
+        temperature = 196.6 + 8.62 * (height / 1000 - 100)
+    else:
+        temperature = 627.6
+    return temperature
+
+
 # cтартовые данные
 height = 0
 speed = 0
-start_temperature = 298.15
-start_mass = 41_420
+start_mass = 41_400
 acceleration = 0
 # константные данные
+Cf = 0.42
 push_force = 490_000
-area = pow(1.85, 2) * cons.pi
+area = pow(1.95, 2) * cons.pi
 nu = 0.029
-parachutes_square_20000 = 3 * pow(1.3, 2) * cons.pi
-parachutes_square_10000 = 3 * pow(7.7, 2) * cons.pi
+parachutes_square_20000 = 3 * pow(0.65, 2) * cons.pi
+parachutes_square_2000 = 3 * pow(1.95, 2) * cons.pi - parachutes_square_20000
 atm_pressure_on_earth = 101_325
 # вспомогательные переменные
 database = []
+parachutes_open_20000 = False
+parachutes_open_2000 = False
 engine_works = True
 booster_detached = False
-parachutes_open_20000 = False
-parachutes_open_10000 = False
 # a = (F тяги / m) - g - F сопрот
 for i in range(1, int(10e4)):
     if (height <= 100 and not(engine_works)):
@@ -32,30 +59,33 @@ for i in range(1, int(10e4)):
     k = 0.25
     cur_time = k * i # current second
     if (engine_works):
-        mass = start_mass - cur_time * 340.142 # current mass
+        mass = start_mass - cur_time * 228 # current mass
     g = cons.G * 5.9722 * 1e24 / pow(6378100 + height, 2) # current gravity acceleration 
-    temperature = start_temperature - 6 * height # current temperature
-    atm_pressure = atm_pressure_on_earth / math.exp(nu * g * height / (cons.R * temperature)) # current atmosphere pressure 
+    temperature = temp_by_elev(height)
+    atm_pressure = atm_pressure_on_earth * math.exp(-nu * g * height / (cons.R * temperature)) # current atmosphere pressure 
     env_density = nu * atm_pressure / (cons.R * temperature)
-    env_resistance_force = area * env_density * pow(speed, 2) / 4
+    env_resistance_force = area * env_density * pow(speed, 2) * 0.5 * Cf
     if (engine_works):
         acceleration = (push_force / mass) - g - (env_resistance_force / mass)
     else:
-        acceleration = -g - (env_resistance_force / mass)
+        if speed > 0:
+            acceleration = -g - (env_resistance_force / mass)
+        else:
+            acceleration = -g + (env_resistance_force / mass)
     speed += k * acceleration
     height += k * speed
     if (not(booster_detached) and height >= 106500):
         mass = 6000
         booster_detached = True
-    if (height <= 20000 and not(parachutes_open_20000) and booster_detached):
-        area += parachutes_square_20000
-        parachutes_open_20000 = True
-    elif (height <= 2500 and not(parachutes_open_10000) and booster_detached):
-        area += parachutes_square_10000 - parachutes_square_20000
-        parachutes_open_10000 = True
+        Cf = 1.15
     # sleep(0.5)
     # print(cur_time, speed, acceleration, height)
-    database.append([cur_time, height])
-
+    database.append([cur_time, speed, acceleration, height, temperature])
+    if (not engine_works and height <= 20000 and not parachutes_open_20000):
+        parachutes_open_20000 = True 
+        area += parachutes_square_20000 * 1.136
+    if (not engine_works and height <= 2000 and not parachutes_open_2000):
+        parachutes_open_2000 = True 
+        area += parachutes_square_2000 * 1.136
 with open('our_data.json', 'w') as f:
     json.dump(database, f)
